8b5ee98f3dc76129a7760b8747fb424e86636cc8
Niklas P <contact@niklasplessing.net>
2025-01-28 15:20:39 +0100

    metadataBase

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,14 @@ import { Toaster } from "@/components/ui/sonner";
 import { Loader } from "@/components/ui/loader";
 
 export const metadata: Metadata = {
-  title: "HeroJam - Home",
-  description: "HeroJam - Home",
+  title: "HeroJam",
+  description:
+    "HeroJam - Demo Terminal App for interacting with the SAGE game engine by Ajuna",
+  metadataBase: new URL(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://herojam.netlify.app/"
+  ),
 };
 
 export default function RootLayout({
